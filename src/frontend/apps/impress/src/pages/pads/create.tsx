1cb138ecb30c0ba21f811e0a865e7fcd54ae834e
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-04-03 11:27:14 +0200

    ✨(app-impress) create feature pads-create
    Create a new feature pads-create for the app impress.
    It will be used to create a pad in the app.

---
+++ src/frontend/apps/impress/src/pages/pads/create.tsx
@@ -0,0 +1,20 @@
+import { ReactElement } from 'react';
+
+import { Box } from '@/components';
+import { CardCreatePad } from '@/features/pads/';
+import { PadLayout } from '@/layouts';
+import { NextPageWithLayout } from '@/types/next';
+
+const Page: NextPageWithLayout = () => {
+  return (
+    <Box className="p-l" $justify="center" $align="start" $height="inherit">
+      <CardCreatePad />
+    </Box>
+  );
+};
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return <PadLayout>{page}</PadLayout>;
+};
+
+export default Page;
