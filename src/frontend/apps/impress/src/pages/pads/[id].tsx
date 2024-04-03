b517b0bc395420ab883fae82bfbd4b78e7e4a40a
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-04-03 11:27:54 +0200

    ✨(app-impress) create feature pad
    Create a new feature pad for the app impress.
    It will be used to display a pad in the app.

---
+++ src/frontend/apps/impress/src/pages/pads/[id].tsx
@@ -0,0 +1,56 @@
+import { Loader } from '@openfun/cunningham-react';
+import { useRouter as useNavigate } from 'next/navigation';
+import { useRouter } from 'next/router';
+import { ReactElement } from 'react';
+
+import { Box } from '@/components';
+import { TextErrors } from '@/components/TextErrors';
+import { usePad } from '@/features/pads';
+import { PadLayout } from '@/layouts';
+import { NextPageWithLayout } from '@/types/next';
+
+const Page: NextPageWithLayout = () => {
+  const {
+    query: { id },
+  } = useRouter();
+
+  if (typeof id !== 'string') {
+    throw new Error('Invalid pad id');
+  }
+
+  return <Pad id={id} />;
+};
+
+interface PadProps {
+  id: string;
+}
+
+const Pad = ({ id }: PadProps) => {
+  const { data: pad, isLoading, isError, error } = usePad({ id });
+  const navigate = useNavigate();
+
+  if (isError && error) {
+    if (error.status === 404) {
+      navigate.replace(`/404`);
+      return null;
+    }
+
+    return <TextErrors causes={error.cause} />;
+  }
+
+  if (isLoading || !pad) {
+    return (
+      <Box $align="center" $justify="center" $height="100%">
+        <Loader />
+      </Box>
+    );
+  }
+
+  return <>Toto</>;
+};
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return <PadLayout>{page}</PadLayout>;
+};
+
+export default Page;
