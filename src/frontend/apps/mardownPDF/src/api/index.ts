9fc7feff941a1150d09839ed5a00f6f52e66c08f
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-04-04 11:47:26 +0200

    🔥(markdownPDF) remove versionning app mardownPDF
    Remove the app mardownPDF from the versionning,
    this app is not used anymore, we don't want
    to maintain it.

--- src/frontend/apps/mardownPDF/src/api/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./useTemplates"
-export * from "./useGeneratePDF"
--- src/frontend/apps/mardownPDF/src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_PUBLIC_API_ROOT_URL: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
