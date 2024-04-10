a5a8a64350dd362817b4afec5c0c763f4079a992
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-04-10 12:25:24 +0200

    ♻️(app-impress) refacto pad store to add the editor
    To spread the data editor to different feature
    of our app, we add the editor in the pad store.
    It will give an easy access to the editor data.

---
+++ src/frontend/apps/impress/src/features/pads/pad/stores/usePadStore.tsx
@@ -0,0 +1,56 @@
+import { BlockNoteEditor } from '@blocknote/core';
+import { WebrtcProvider } from 'y-webrtc';
+import * as Y from 'yjs';
+import { create } from 'zustand';
+
+import { Pad } from '../types';
+
+export interface PadStore {
+  padsStore: {
+    [padId: Pad['id']]: {
+      provider: WebrtcProvider;
+      editor?: BlockNoteEditor;
+    };
+  };
+  createProvider: (padId: Pad['id']) => WebrtcProvider;
+  setEditor: (padId: Pad['id'], editor: BlockNoteEditor) => void;
+}
+
+const initialState = {
+  padsStore: {},
+};
+
+export const usePadStore = create<PadStore>((set) => ({
+  padsStore: initialState.padsStore,
+  createProvider: (padId: string) => {
+    const provider = new WebrtcProvider(padId, new Y.Doc(), {
+      signaling: [process.env.NEXT_PUBLIC_SIGNALING_URL || ''],
+    });
+
+    set(({ padsStore }) => {
+      return {
+        padsStore: {
+          ...padsStore,
+          [padId]: {
+            provider,
+          },
+        },
+      };
+    });
+
+    return provider;
+  },
+  setEditor: (padId, editor) => {
+    set(({ padsStore }) => {
+      return {
+        padsStore: {
+          ...padsStore,
+          [padId]: {
+            ...padsStore[padId],
+            editor,
+          },
+        },
+      };
+    });
+  },
+}));
