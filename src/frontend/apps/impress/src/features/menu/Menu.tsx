d967896874f384c8eed12714b2dc6fe33a8d73f7
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-05-02 13:50:49 +0200

    🔥(app-impress) remove templates
    We will generate the templates internally.
    We remove the template editor.

--- src/frontend/apps/impress/src/features/menu/Menu.tsx
+++ src/frontend/apps/impress/src/features/menu/Menu.tsx
@@ -6,7 +6,6 @@ import useCunninghamTheme from '@/cunningham/useCunninghamTheme';
 
 import MenuItem from './MenuItems';
 import IconPad from './assets/icon-pad.svg';
-import IconTemplate from './assets/icon-template.svg';
 
 export const Menu = () => {
   const { colorsTokens } = useCunninghamTheme();
@@ -22,11 +21,6 @@ export const Menu = () => {
     >
       <Box className="pt-l" $direction="column" $gap="0.8rem">
         <MenuItem Icon={IconPad} label={t('Pad')} href="/" alias={['/pads/']} />
-        <MenuItem
-          Icon={IconTemplate}
-          label={t('Template')}
-          href="/templates/"
-        />
       </Box>
     </Box>
   );
