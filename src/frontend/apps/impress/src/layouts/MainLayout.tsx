d0fa8370b9be3b7048773214101be149972d61cd
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-05-14 09:40:56 +0200

    🔥(app-impress) remove menu
    For the moment we only have 1 menu item,
    so we can remove the menu.

--- src/frontend/apps/impress/src/layouts/MainLayout.tsx
+++ src/frontend/apps/impress/src/layouts/MainLayout.tsx
@@ -1,13 +1,11 @@
 import { Box } from '@/components';
 import { HEADER_HEIGHT, Header } from '@/features/header';
-import { Menu } from '@/features/menu';
 
 export function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <Box $height="100vh" $css="overflow:hidden;">
       <Header />
       <Box $css="flex: 1;" $direction="row">
-        <Menu />
         <Box as="main" $height={`calc(100vh - ${HEADER_HEIGHT})`} $width="100%">
           {children}
         </Box>
