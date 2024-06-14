4f48d71f7575deb929a12acfcfb068d214a3f4fc
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-06-14 16:12:39 +0200

    🧑‍💻(frontend) env var service-worker dev mode
    Add a new environment variable to enable or disable
    the service worker in development mode.
    By default, the service worker is disabled
    in development mode, it can cause problems
    when developing the application with the hmr.
    It can creates useless log in the console
    as well.
    We can easily enable it by setting
    NEXT_PUBLIC_SW_DEACTIVATED to false in the
    .env.development file.
    We will use this new var to not use the service
    worder with the CI as well.

---
+++ src/frontend/apps/impress/src/core/service-worker/__tests__/_app.test.tsx
@@ -0,0 +1,71 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { Router } from 'next/router';
+import { PropsWithChildren, ReactElement } from 'react';
+
+import App from '@/pages/_app';
+import { AppWrapper } from '@/tests/utils';
+import { NextPageWithLayout } from '@/types/next';
+
+const Page: NextPageWithLayout = () => {
+  return <div>Test Page</div>;
+};
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return <div>{page}</div>;
+};
+
+jest.mock('@/core/', () => ({
+  ...jest.requireActual('@/core/'),
+  AppProvider: ({ children }: PropsWithChildren) => <div>{children}</div>,
+}));
+
+describe('App', () => {
+  it('checks service-worker is register', () => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+
+    const registerSpy = jest.fn();
+    registerSpy.mockImplementation(
+      () =>
+        new Promise((reject) => {
+          reject('error');
+        }),
+    );
+    Object.defineProperty(navigator, 'serviceWorker', {
+      value: {
+        register: registerSpy,
+      },
+      writable: true,
+    });
+
+    render(<App Component={Page} router={{} as Router} pageProps={{}} />, {
+      wrapper: AppWrapper,
+    });
+
+    expect(registerSpy).toHaveBeenCalledWith('/service-worker.js');
+  });
+
+  it('checks service-worker is not register', () => {
+    process.env.NEXT_PUBLIC_SW_DEACTIVATED = 'true';
+
+    const registerSpy = jest.fn();
+    registerSpy.mockImplementation(
+      () =>
+        new Promise((reject) => {
+          reject('error');
+        }),
+    );
+    Object.defineProperty(navigator, 'serviceWorker', {
+      value: {
+        register: registerSpy,
+      },
+      writable: true,
+    });
+
+    render(<App Component={Page} router={{} as Router} pageProps={{}} />, {
+      wrapper: AppWrapper,
+    });
+
+    expect(registerSpy).not.toHaveBeenCalledWith('/service-worker.js');
+  });
+});
