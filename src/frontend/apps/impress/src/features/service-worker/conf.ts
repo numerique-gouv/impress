9ae95e09c7375cb301a99eaa1d090a3c0eb03a83
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-07-02 15:53:30 +0200

    🐛(service-worker) networkFirst on api request
    Some api requests were served by the cache first,
    which caused the data to be outdated.
    This change makes sure that the api requests
    are always served by the network first.

--- src/frontend/apps/impress/src/features/service-worker/conf.ts
+++ src/frontend/apps/impress/src/features/service-worker/conf.ts
@@ -1,5 +1,16 @@
+import pkg from '@/../package.json';
+
 export const SW_DEV_URL = [
   'http://localhost:3000',
   'https://impress.127.0.0.1.nip.io',
   'https://impress-staging.beta.numerique.gouv.fr',
 ];
+
+export const SW_DEV_API = 'http://localhost:8071';
+
+export const SW_VERSION = `v-${process.env.NEXT_PUBLIC_BUILD_ID}`;
+
+export const DAYS_EXP = 5;
+
+export const getCacheNameVersion = (cacheName: string) =>
+  `${pkg.name}-${cacheName}-${SW_VERSION}`;
