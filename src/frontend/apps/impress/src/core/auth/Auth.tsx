bcf5b7b2d162728c3e09887fd22ee5b6a684575e
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-07-02 10:06:19 +0200

    🔥(frontend) remove coming-soon
    Remove the coming-soon page and the redirection to it.

--- src/frontend/apps/impress/src/core/auth/Auth.tsx
+++ src/frontend/apps/impress/src/core/auth/Auth.tsx
@@ -1,35 +1,17 @@
 import { Loader } from '@openfun/cunningham-react';
-import { usePathname, useRouter } from 'next/navigation';
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 
 import { Box } from '@/components';
 
 import { useAuthStore } from './useAuthStore';
 export const Auth = ({ children }: PropsWithChildren) => {
   const { authenticated, initAuth } = useAuthStore();
-  const router = useRouter();
-  const [isDomainComingSoon, setIsDomainComingSoon] = useState(false);
-  const domainComingSoon = 'docs.numerique.gouv.fr';
-  const pathComingSoon = '/coming-soon/';
-  const pathname = usePathname();
 
   useEffect(() => {
-    if (window.location.origin.includes(domainComingSoon)) {
-      router.push(pathComingSoon);
-      return;
-    }
-
     initAuth();
-  }, [initAuth, router]);
+  }, [initAuth]);
 
-  useEffect(() => {
-    setIsDomainComingSoon(
-      window.location.origin.includes(domainComingSoon) &&
-        pathname === pathComingSoon,
-    );
-  }, [pathname]);
-
-  if (!authenticated && !isDomainComingSoon) {
+  if (!authenticated) {
     return (
       <Box $height="100vh" $width="100vw" $align="center" $justify="center">
         <Loader />
