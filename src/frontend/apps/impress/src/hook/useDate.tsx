512e03207e8cbd6f7fcd0d1642015188ecf86645
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-07-09 18:09:47 +0200

    ✨(frontend) create hook useDate
    Create hook useDate, it is an date helper.
    We use it for now to format date.

---
+++ src/frontend/apps/impress/src/hook/useDate.tsx
@@ -0,0 +1,22 @@
+import { DateTime, DateTimeFormatOptions } from 'luxon';
+import { useTranslation } from 'react-i18next';
+
+const format: DateTimeFormatOptions = {
+  month: '2-digit',
+  day: '2-digit',
+  year: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+};
+
+export const useDate = () => {
+  const { i18n } = useTranslation();
+
+  const formatDate = (date: string): string => {
+    return DateTime.fromISO(date)
+      .setLocale(i18n.language)
+      .toLocaleString(format);
+  };
+
+  return { formatDate };
+};
