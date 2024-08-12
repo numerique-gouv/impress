c724ae02535708878a6ea1975d5bb234ecce91dc
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-08-12 16:39:36 +0200

    🔥(project) remove tsclient
    We don't need the tsclient for the moment.
    Better to remove it.

--- src/tsclient/scripts/openapi-typescript-codegen/generate_api_client_local.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# usage: yarn generate:api:client:local [--output]
-
-# OPTIONS:
-#  --output  the path folder where types will be generated
-
-openapi --input http://app-dev:8000/v1.0/swagger.json --output $1 --indent='2' --name ApiClientImpress --useOptions
