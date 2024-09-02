7b11cc334fbc23f9d07fef769c17a101d71d3bfd
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-09-02 13:53:25 +0200

    ♻️(frontend) open version panel from docs options
    Versions panel is a feature that will not be used
    by all users, so it should be hidden
    by default. The user can open it from the docs
    options.

--- src/frontend/apps/impress/src/features/docs/doc-versioning/components/Panel.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-versioning/components/Panel.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Box, Card, IconBG, Text } from '@/components';
 import { useCunninghamTheme } from '@/cunningham';
 import { Doc } from '@/features/docs/doc-management';
+
+import { useDocVersionStore } from '../stores';
 
 import { VersionList } from './VersionList';
 
@@ -14,8 +16,13 @@ interface PanelProps {
 export const Panel = ({ doc }: PanelProps) => {
   const { t } = useTranslation();
   const { colorsTokens } = useCunninghamTheme();
+  const { setIsPanelOpen } = useDocVersionStore();
 
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(true);
+  }, []);
 
   const closedOverridingStyles = !isOpen && {
     $width: '0',
@@ -46,28 +53,6 @@ export const Panel = ({ doc }: PanelProps) => {
       aria-label={t('Document version panel')}
       {...closedOverridingStyles}
     >
-      <IconBG
-        iconName="menu_open"
-        aria-label={
-          isOpen
-            ? t('Close the document version panel')
-            : t('Open the document version panel')
-        }
-        $background="transparent"
-        $size="h2"
-        $zIndex={1}
-        $css={`
-          cursor: pointer;
-          left: ${isOpen ? '0' : '-3.3'}rem;
-          top: 0.1rem;
-          transform: rotate(${isOpen ? '180' : '0'}deg);
-          transition: ${transition};
-          user-select: none;
-        `}
-        $position="absolute"
-        onClick={() => setIsOpen(!isOpen)}
-        $radius="2px"
-      />
       <Box
         $overflow="hidden"
         $css={`
@@ -82,6 +67,34 @@ export const Panel = ({ doc }: PanelProps) => {
           $justify="center"
           $css={`border-top: 2px solid ${colorsTokens()['primary-600']};`}
         >
+          <IconBG
+            iconName="menu_open"
+            aria-label={
+              isOpen
+                ? t('Close the document version panel')
+                : t('Open the document version panel')
+            }
+            $background="transparent"
+            $size="h2"
+            $zIndex={1}
+            $css={`
+              cursor: pointer;
+              left: 0rem;
+              top: 0.1rem;
+              transition: ${transition};
+              transform: rotate(180deg);
+              opacity: ${isOpen ? '1' : '0'};
+              user-select: none;
+            `}
+            $position="absolute"
+            onClick={() => {
+              setIsOpen(false);
+              setTimeout(() => {
+                setIsPanelOpen(false);
+              }, 400);
+            }}
+            $radius="2px"
+          />
           <Text $weight="bold" $size="l" $theme="primary">
             {t('VERSIONS')}
           </Text>
