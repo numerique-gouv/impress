c7439be1a5f54d2e351d85a55ccfc8661429380f
Raito Bezarius <masterancpp@gmail.com>
2024-09-12 15:40:11 +0200

    feat(api,front): expose the `sub` OIDC field for the frontend
    Useful for E2EE encryption identification.
    
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- src/frontend/apps/impress/src/core/auth/api/types.ts
+++ src/frontend/apps/impress/src/core/auth/api/types.ts
@@ -2,10 +2,12 @@
  * Represents user retrieved from the API.
  * @interface User
  * @property {string} id - The id of the user.
+ * @property {string} sub - The `sub` field of OIDC
  * @property {string} email - The email of the user.
  * @property {string} name - The name of the user.
  */
 export interface User {
   id: string;
+  sub: string;
   email: string;
 }
