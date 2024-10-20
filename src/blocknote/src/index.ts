302e5a503f0c452e58abe93b5fbd613376de879e
lebaudantoine <lebaud.antoine131@gmail.com>
2024-10-21 00:57:08 +0200

    wip introduce convert logic

--- src/blocknote/src/index.ts
+++ src/blocknote/src/index.ts
@@ -1,23 +1,37 @@
 import express, { Express, Request, Response } from 'express'
+import { asyncWrapper, convertMarkdown } from './utils'
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
 
 dotenv.config()
 
 const app: Express = express()
+const router = express.Router()
 const port = process.env.PORT ?? 8081
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Logging middleware, logs the request method and path for each incoming request
+router.use(async function (req, res, next) {
+  console.log(`/${req.method}`)
+  next()
 })
 
-app.get('/__heartbeat__', (req: Request, res: Response) => {
+// Liveness probe endpoint for Kubernetes health checks
+router.get('/__heartbeat__', (req: Request, res: Response) => {
   res.status(200).send({ status: 'OK' })
 })
 
-app.get('/__lbheartbeat__', (req: Request, res: Response) => {
+// Load balancer heartbeat check, useful to detect app readiness
+router.get('/__lbheartbeat__', (req: Request, res: Response) => {
   res.status(200).send({ status: 'OK' })
 })
+
+router.post('/', asyncWrapper(convertMarkdown))
+
+app.use('/', router)
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`)
+  console.log(`[server]: Server listening on port ${port}`)
 })
