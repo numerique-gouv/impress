a1c497ef276951be640f0bb5a093d10a3e99189f
lebaudantoine <lebaud.antoine131@gmail.com>
2024-10-21 00:52:31 +0200

    wip install prettier and format

--- src/blocknote/src/index.ts
+++ src/blocknote/src/index.ts
@@ -1,23 +1,23 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = process.env.PORT ?? 8081;
+const app: Express = express()
+const port = process.env.PORT ?? 8081
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server')
+})
 
-app.get("/__heartbeat__", (req: Request, res: Response) => {
-  res.status(200).send({ status: "OK" });
-});
+app.get('/__heartbeat__', (req: Request, res: Response) => {
+  res.status(200).send({ status: 'OK' })
+})
 
-app.get("/__lbheartbeat__", (req: Request, res: Response) => {
-  res.status(200).send({ status: "OK" });
-});
+app.get('/__lbheartbeat__', (req: Request, res: Response) => {
+  res.status(200).send({ status: 'OK' })
+})
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+})
