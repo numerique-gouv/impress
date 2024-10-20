89a44e4979d769c1e096015307e6403a5c1feef0
lebaudantoine <lebaud.antoine131@gmail.com>
2024-10-20 23:49:25 +0200

    add blocknote micro service to kube

--- src/blocknote/src/index.ts
+++ src/blocknote/src/index.ts
@@ -10,6 +10,14 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
+app.get("/__heartbeat__", (req: Request, res: Response) => {
+  res.status(200).send({ status: "OK" });
+});
+
+app.get("/__lbheartbeat__", (req: Request, res: Response) => {
+  res.status(200).send({ status: "OK" });
+});
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
