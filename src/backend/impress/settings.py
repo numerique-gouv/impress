60a66e878356b349eb4ba5f09bf28020607f8f57
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-11-12 13:00:24 +0100

    🧐(backend) adapt Sentry config
    We add SENTRY_ENV var, to be able to distinguish
    errors between different environments in Sentry
    (staging / preprod / prod).

--- src/backend/impress/settings.py
+++ src/backend/impress/settings.py
@@ -372,7 +372,8 @@ class Base(Configuration):
     CORS_ALLOWED_ORIGIN_REGEXES = values.ListValue([])
 
     # Sentry
-    SENTRY_DSN = values.Value(None, environ_name="SENTRY_DSN")
+    SENTRY_DSN = values.Value(None, environ_name="SENTRY_DSN", environ_prefix=None)
+    SENTRY_ENV = values.Value(None, environ_name="SENTRY_ENV", environ_prefix=None)
 
     # Easy thumbnails
     THUMBNAIL_EXTENSION = "webp"
@@ -524,7 +525,7 @@ class Base(Configuration):
         if cls.SENTRY_DSN is not None:
             sentry_sdk.init(
                 dsn=cls.SENTRY_DSN,
-                environment=cls.__name__.lower(),
+                environment=cls.SENTRY_ENV or cls.__name__.lower(),
                 release=get_release(),
                 integrations=[DjangoIntegration()],
             )
