077d1a84955884bfc1ae781c3de8c0d2be6d187c
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-11-15 14:34:45 +0100

    ✨(frontend) add sentry
    In order to monitor the frontend, we are adding
    sentry.

---
+++ src/frontend/apps/impress/src/stores/useSentryStore.tsx
@@ -0,0 +1,32 @@
+import * as Sentry from '@sentry/nextjs';
+import type { Client } from '@sentry/types';
+import { create } from 'zustand';
+
+import packageJson from '../../package.json';
+
+interface SentryState {
+  sentry?: Client;
+  setSentry: (dsn?: string, environment?: string) => void;
+}
+
+export const useSentryStore = create<SentryState>((set, get) => ({
+  sentry: undefined,
+  setSentry: (dsn, environment) => {
+    const sentry = get().sentry;
+    if (sentry) {
+      return;
+    }
+
+    set({
+      sentry: Sentry.init({
+        dsn,
+        environment,
+        integrations: [Sentry.replayIntegration()],
+        release: packageJson.version,
+        replaysSessionSampleRate: 0.1,
+        replaysOnErrorSampleRate: 1.0,
+        tracesSampleRate: 1.0,
+      }),
+    });
+  },
+}));
