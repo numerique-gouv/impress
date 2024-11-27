6dd16979150939b1e4ec82ba527ebbdb73237491
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2024-11-27 11:35:49 +0100

    🐛(frontend) use hook useTranslation
    Sentry highlitghted a few errors about the
    function "t" not being defined. Better to get
    it from the hook useTranslation.

--- src/frontend/apps/impress/src/features/docs/doc-management/components/ModalRemoveDoc.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-management/components/ModalRemoveDoc.tsx
@@ -6,8 +6,8 @@ import {
   VariantType,
   useToastProvider,
 } from '@openfun/cunningham-react';
-import { t } from 'i18next';
 import { useRouter } from 'next/router';
+import { useTranslation } from 'react-i18next';
 
 import { Box, Text, TextErrors } from '@/components';
 import { useCunninghamTheme } from '@/cunningham/';
@@ -22,6 +22,7 @@ interface ModalRemoveDocProps {
 }
 
 export const ModalRemoveDoc = ({ onClose, doc }: ModalRemoveDocProps) => {
+  const { t } = useTranslation();
   const { colorsTokens } = useCunninghamTheme();
   const { toast } = useToastProvider();
   const { push } = useRouter();
