f2d1fc14c72475a0a491b4bc6dad4bb7df7281c5
Nathan Panchout <nathanpanchout@hotmail.com>
2024-12-09 18:00:31 +0100

    ✨(frontend) add Quick Search component suite
    - Introduced a new Quick Search feature with multiple components
    - Implemented styling for the Quick Search components to
    ensure a cohesive look and feel across the application.

---
+++ src/frontend/apps/impress/src/components/quick-search/QuickSearchItemContent.tsx
@@ -0,0 +1,44 @@
+import { ReactNode } from 'react';
+
+import { useCunninghamTheme } from '@/cunningham';
+
+import { Box } from '../Box';
+
+export type QuickSearchItemContentProps = {
+  alwaysShowRight?: boolean;
+  left: ReactNode;
+  right?: ReactNode;
+};
+
+export const QuickSearchItemContent = ({
+  alwaysShowRight = false,
+  left,
+  right,
+}: QuickSearchItemContentProps) => {
+  const { spacingsTokens } = useCunninghamTheme();
+  const spacings = spacingsTokens();
+
+  return (
+    <Box
+      $direction="row"
+      $align="center"
+      $padding={{ horizontal: '2xs', vertical: '3xs' }}
+      $justify="space-between"
+      $width="100%"
+    >
+      <Box $direction="row" $align="center" $gap={spacings['2xs']}>
+        {left}
+      </Box>
+
+      {right && (
+        <Box
+          className={!alwaysShowRight ? 'show-right-on-focus' : ''}
+          $direction="row"
+          $align="center"
+        >
+          {right}
+        </Box>
+      )}
+    </Box>
+  );
+};
