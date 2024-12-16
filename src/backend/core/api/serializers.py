a12834c706a14b2ff052faf4a19d2847c1a73d8e
Samuel Paccoud - DINUM <samuel.paccoud@mail.numerique.gouv.fr>
2024-12-16 16:58:14 +0100

    ✨(backend) add django-treebeard to allow tree structure on documents
    We choose to use Django-treebeard for its quality, performance and
    stability. Adding tree structure to documents is as simple as
    inheriting from the MP_Node class.

--- src/backend/core/api/serializers.py
+++ src/backend/core/api/serializers.py
@@ -201,7 +201,7 @@ class DocumentSerializer(ListDocumentSerializer):
             "abilities",
             "created_at",
             "creator",
-            "is_avorite",
+            "is_favorite",
             "link_role",
             "link_reach",
             "nb_accesses",
@@ -281,7 +281,7 @@ class ServerCreateDocumentSerializer(serializers.Serializer):
         except ConversionError as err:
             raise exceptions.APIException(detail="could not convert content") from err
 
-        document = models.Document.objects.create(
+        document = models.Document.add_root(
             title=validated_data["title"],
             content=document_content,
             creator=user,
