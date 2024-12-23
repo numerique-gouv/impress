3bea1321407447c27d4aec9643387ff4b3c7a482
Nathan Panchout <nathanpanchout@hotmail.com>
2024-12-23 10:20:40 +0100

    ✨(frontend) enhance left panel components
    - Updated padding and radius styles in LeftPanelTargetFilters and
    LeftPanelFavorites for improved layout consistency.
    - Introduced LeftPanelDocContent component to display document details
    when navigating to specific documentation pages.
    - Enhanced LeftPanelContent to conditionally render LeftPanelDocContent
    based on the current route.
    - Adjusted LeftPanelHeader button colors for better visual hierarchy.
    - Refactored MainLayout padding for a more responsive design.

--- src/frontend/apps/impress/src/features/left-panel/components/LeftPanelContent.tsx
+++ src/frontend/apps/impress/src/features/left-panel/components/LeftPanelContent.tsx
@@ -4,11 +4,13 @@ import { css } from 'styled-components';
 import { Box, SeparatedSection } from '@/components';
 
 import { LeftPanelTargetFilters } from './LefPanelTargetFilters';
+import { LeftPanelDocContent } from './LeftPanelDocContent';
 import { LeftPanelFavorites } from './LeftPanelFavorites';
 
 export const LeftPanelContent = () => {
   const router = useRouter();
   const isHome = router.pathname === '/';
+  const isDoc = router.pathname === '/docs/[id]';
 
   return (
     <>
@@ -24,13 +26,16 @@ export const LeftPanelContent = () => {
               <LeftPanelTargetFilters />
             </SeparatedSection>
           </Box>
-          <Box $flex={1} $css="overflow-y: auto; overflow-x: hidden;">
-            <SeparatedSection showSeparator={false}>
-              <LeftPanelFavorites />
-            </SeparatedSection>
+          <Box
+            $flex={1}
+            $width="100%"
+            $css="overflow-y: auto; overflow-x: hidden;"
+          >
+            <LeftPanelFavorites />
           </Box>
         </>
       )}
+      {isDoc && <LeftPanelDocContent />}
     </>
   );
 };
--- src/frontend/apps/impress/src/features/left-panel/components/LeftPanelFavorites.tsx
+++ src/frontend/apps/impress/src/features/left-panel/components/LeftPanelFavorites.tsx
@@ -1,7 +1,13 @@
 import { useTranslation } from 'react-i18next';
 import { css } from 'styled-components';
 
-import { Box, InfiniteScroll, StyledLink, Text } from '@/components';
+import {
+  Box,
+  InfiniteScroll,
+  SeparatedSection,
+  StyledLink,
+  Text,
+} from '@/components';
 import { useCunninghamTheme } from '@/cunningham';
 import { useInfiniteDocs } from '@/features/docs';
 import { SimpleDocItem } from '@/features/docs/docs-grid/components/SimpleDocItem';
@@ -24,39 +30,46 @@ export const LeftPanelFavorites = () => {
   }
 
   return (
-    <Box
-      $justify="center"
-      $padding={{ horizontal: 'xs' }}
-      $gap={spacing['2xs']}
-      $height="100%"
-      data-testid="left-panel-favorites"
-    >
-      <Text $size="sm" $variation="700" $weight="700">
-        {t('Pinned documents')}
-      </Text>
-      <InfiniteScroll
-        hasMore={docs.hasNextPage}
-        isLoading={docs.isFetchingNextPage}
-        next={() => void docs.fetchNextPage()}
+    <SeparatedSection showSeparator={true}>
+      <Box
+        $justify="center"
+        $padding={{ horizontal: 'sm' }}
+        $gap={spacing['2xs']}
+        $height="100%"
+        data-testid="left-panel-favorites"
       >
-        {invitations.map((doc) => (
-          <Box
-            $css={css`
-              padding: ${spacing['2xs']};
-              border-radius: 4px;
-              &:hover {
-                cursor: pointer;
-                background-color: var(--c--theme--colors--greyscale-100);
-              }
-            `}
-            key={doc.id}
-          >
-            <StyledLink href={`/docs/${doc.id}`}>
-              <SimpleDocItem showAccesses doc={doc} />
-            </StyledLink>
-          </Box>
-        ))}
-      </InfiniteScroll>
-    </Box>
+        <Text
+          $size="sm"
+          $variation="700"
+          $padding={{ horizontal: '3xs' }}
+          $weight="700"
+        >
+          {t('Pinned documents')}
+        </Text>
+        <InfiniteScroll
+          hasMore={docs.hasNextPage}
+          isLoading={docs.isFetchingNextPage}
+          next={() => void docs.fetchNextPage()}
+        >
+          {invitations.map((doc) => (
+            <Box
+              $css={css`
+                padding: ${spacing['2xs']};
+                border-radius: 4px;
+                &:hover {
+                  cursor: pointer;
+                  background-color: var(--c--theme--colors--greyscale-100);
+                }
+              `}
+              key={doc.id}
+            >
+              <StyledLink href={`/docs/${doc.id}`}>
+                <SimpleDocItem showAccesses doc={doc} />
+              </StyledLink>
+            </Box>
+          ))}
+        </InfiniteScroll>
+      </Box>
+    </SeparatedSection>
   );
 };
