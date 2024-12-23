bba2804cffef55283c910ad90b0d0b10ceec1b0f
Nathan Panchout <nathanpanchout@hotmail.com>
2024-12-23 10:29:44 +0100

    ✨(frontend) enhance QuickSearch components
    - Updated QuickSearchItemContent to ensure full width for better layout
    consistency.
    - Adjusted padding in QuickSearchStyle for improved spacing and visual
    hierarchy.
    - Refactored DocSearchItem to utilize Box component for consistent
    styling and layout.
    - Removed unused imports in DocSearchItem to streamline the codebase.

--- src/frontend/apps/impress/src/features/docs/doc-search/components/DocSearchItem.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-search/components/DocSearchItem.tsx
@@ -1,6 +1,4 @@
-import { DateTime } from 'luxon';
-
-import { Box, Icon, Text } from '@/components';
+import { Box, Icon } from '@/components';
 import { QuickSearchItemContent } from '@/components/quick-search/QuickSearchItemContent';
 import { Doc } from '@/features/docs/doc-management';
 import { SimpleDocItem } from '@/features/docs/docs-grid/components/SimpleDocItem';
@@ -13,26 +11,19 @@ type DocSearchItemProps = {
 export const DocSearchItem = ({ doc }: DocSearchItemProps) => {
   const { isDesktop } = useResponsiveStore();
   return (
-    <div data-testid={`doc-search-item-${doc.id}`}>
+    <Box data-testid={`doc-search-item-${doc.id}`} $width="100%">
       <QuickSearchItemContent
         left={
-          <Box $direction="row" $align="center" $gap="10px">
+          <Box $direction="row" $align="center" $gap="10px" $width="100%">
             <Box $flex={isDesktop ? 9 : 1}>
               <SimpleDocItem doc={doc} showAccesses />
             </Box>
-            {isDesktop && (
-              <Box $flex={2} $justify="center" $align="center">
-                <Text $variation="500" $align="right" $size="xs">
-                  {DateTime.fromISO(doc.updated_at).toRelative()}
-                </Text>
-              </Box>
-            )}
           </Box>
         }
         right={
           <Icon iconName="keyboard_return" $theme="primary" $variation="800" />
         }
       />
-    </div>
+    </Box>
   );
 };
