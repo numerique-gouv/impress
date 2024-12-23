765b4d6d2859744682b1778ebec7c051bc07c8f2
Nathan Panchout <nathanpanchout@hotmail.com>
2024-12-23 10:31:57 +0100

    ✨(frontend) enhance document editor and header components
    - Improved styling for headings in BlockNoteEditor for better visual
    hierarchy.
    - Adjusted padding in DocEditor and DocHeader based on device type for
    responsive design.
    - Updated DocTitle and ModalExport components to enhance typography and
    spacing.
    - Refactored DocToolBox to improve share button functionality and access
    display.
    - Enhanced versioning modal with better layout and accessibility
    features.
    - Cleaned up unused imports and optimized component structures for
    maintainability.

--- src/frontend/apps/impress/src/features/docs/doc-editor/components/BlockNoteEditor.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-editor/components/BlockNoteEditor.tsx
@@ -24,6 +24,49 @@ const cssEditor = (readonly: boolean) => `
   &, & > .bn-container, & .ProseMirror {
     height:100%;
     
+      .bn-side-menu[data-block-type=heading][data-level="1"] { 
+         height: 50px; 
+     } 
+     .bn-side-menu[data-block-type=heading][data-level="2"] { 
+       height: 43px; 
+     } 
+    .bn-side-menu[data-block-type=heading][data-level="3"] {
+      height: 35px;
+    }
+    h1 {
+      font-size: 1.875rem;
+    }
+    h2 {
+      font-size: 1.5rem;
+    }
+    h3 {
+      font-size: 1.25rem;
+    }
+    a {
+      color: var(--c--theme--colors--greyscale-500);
+      cursor: pointer;
+    }
+    .bn-block-group
+      .bn-block-group
+      .bn-block-outer:not([data-prev-depth-changed]):before {
+      border-left: none;
+    }
+  }
+  
+  .bn-editor {
+    
+    color: var(--c--theme--colors--greyscale-700);
+  }
+  .bn-block-outer:not(:first-child) {
+    &:has(h1) {
+      padding-top: 32px;
+    }
+    &:has(h2) {
+      padding-top: 24px;
+    }
+    &:has(h3) {
+      padding-top: 16px;
+    }
   };
   
   & .bn-inline-content code {
